e291488fe68385a9d7a998b06622a3ae2910462a
SeungwonYang <65176721+SeungWonYang-pro@users.noreply.github.com>
2023-03-09 21:16:00 +0900

    first git assignment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from typing import List
 def twoSum(nums: List[int], target: int) -> List[int]:
         ans = [0,0]
-        st = 0
-        ed = 0
         cursum = nums[0]
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
