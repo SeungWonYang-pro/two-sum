8f82158f1f119d67b8bba940c83fb81398c4b1e3
SeungwonYang <65176721+SeungWonYang-pro@users.noreply.github.com>
2023-03-09 21:14:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 from typing import List
 def twoSum(nums: List[int], target: int) -> List[int]:
-    ans = [0,0]
-    st = 0
-    ed = 0
-    cursum = nums[0]
-    while 1:
-        if(cursum==target):
-            break
-        if(st==ed):
-            ed=ed+1
-            cursum = cursum + nums[ed]
-        else:
-            if(cursum < target):
-                ed=ed+1
-                cursum = cursum + nums[ed]
-            if(cursum > target):
-                cursum = cursum - nums[st]
-                st = st+1
-        
-        
-    ans[0]= st
-    ans[1] = ed
-    return ans
+        ans = [0,0]
+        st = 0
+        ed = 0
+        cursum = nums[0]
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if((nums[i]+nums[j]) == target):
+                    ans[0]=i
+                    ans[1]=j
+                    break
+
+        return ans
